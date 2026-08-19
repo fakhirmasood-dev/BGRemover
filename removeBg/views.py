def9e24c9e0d4bb053306c86272e13990feadcca
Fakhirktk49 <fakhirmasood0@gmail.com>
2026-08-19 08:14:57 +0500

    settings done for saving image locally

--- removeBg/views.py
+++ removeBg/views.py
@@ -4,7 +4,7 @@ import json
 def home(request):
     if request.method =='POST':
         file=request.FILES.get('file')
-        print(file)
+        
     return render(request,'removeBg/html/main.html')
 
 # def get_img(request):
