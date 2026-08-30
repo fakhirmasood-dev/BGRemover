bc1764ecd36d34a3032193e9c3e2b8fc6e74a72e
fakhirmasood-dev <fakhirmasood0@gmail.com>
2026-08-30 08:09:08 +0500

    logging functionality added

--- bgremover/settings.py
+++ bgremover/settings.py
@@ -129,3 +129,38 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+LOGGING={
+    'version':1,
+    'disable_default_loggers':False,
+
+    'formatters':{
+        'standard':{
+            'format':' {levelname} {filename} {name} {lineno} {asctime} {module} {pathname} {funcName} {message}',
+            'style':'{'
+        },
+        'simple': { 
+            'format':'{levelname} {lineno} {asctime} {message}',
+            'style':'{'
+        }
+    },
+
+    'handlers':{
+        'file':{
+            'class':'logging.FileHandler',
+            'level':'INFO',
+            'filename':BASE_DIR / 'file_logs/logs.log',
+            'formatter':'standard',
+
+         }
+     
+    },
+
+     'loggers':{
+         'django':{
+            'handlers':['file'],
+            'level':'INFO',
+             'propagate':False
+         }
+     }
+}
